28a8364920c30ddffea87d204d98e24e72ef0392
Erik Gomes <gomeserik35@gmail.com>
2024-10-15 18:17:11 -0300

    Implementando função

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 import prompt from 'prompt-sync'
-var prompt = prompt()
+let chat = prompt()
 
+let vitorias = chat('Digite o número de vitórias: ')
+let derrotas = chat('Digite o número de derrotas: ')
+
+let elo = ""
+let saldoJogador = saldoVitorias(vitorias, derrotas)
+
+function saldoVitorias(vitorias, derrotas){
+    return vitorias-derrotas
+}
+
+
+if(saldoJogador<=10){
+    elo = "Ferro"
+}else if(saldoJogador<=20){
+    elo = "Bronze"
+}else if(saldoJogador<=50){
+    elo = "Prata"
+}else if(saldoJogador<=80){
+    elo = "Ouro"
+}else if(saldoJogador<=90){
+    elo = "Diamante"
+}else if(saldoJogador<=100){
+    elo = "Lendário"
+}else{
+    elo = "Imortal"
+}
+
+console.log(`O Herói tem de saldo ${saldoJogador} está no nível de ${elo}.`)
